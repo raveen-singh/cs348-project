ef47675a20a52a1c290ddafd51dcb4984caa5149
raveen-singh <18638226+raveen-singh@users.noreply.github.com>
2022-10-18 13:07:35 -0400

    table setup

--- api/setup.sql
+++ api/setup.sql
@@ -1,13 +1,56 @@
-CREATE DATABASE cs348db;
+CREATE DATABASE IF NOT EXISTS cs348db;
 USE cs348db;
 
-CREATE TABLE users(
-    firstname VARCHAR(30) NOT NULL,  
-    lastname VARCHAR(30) NOT NULL);
+CREATE TABLE IF NOT EXISTS UnitListerAccount(
+    pm_id int NOT NULL,  
+    name VARCHAR(255) NOT NULL,
+    phone_num VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    website VARCHAR(255) NOT NULL,
+    PRIMARY KEY (pm_id)
+    );
 
-INSERT INTO users VALUE(
-    "cs348db",
-    " connected"
+CREATE TABLE IF NOT EXISTS Building(
+    building_id int NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    pet_friendly TINYINT(1) NOT NULL, -- TINYINT functions as a boolean
+    laundry_availability VARCHAR(255) NOT NULL, -- Do we want a yes or no, or ensuite vs in building
+    type_of_unit VARCHAR(255) NOT NULL, -- use a multiselect on frontend so values are consistent
+    distance_from_waterloo DECIMAL(3,1) NOT NULL,
+    PRIMARY KEY (building_id)
 );
 
-DESCRIBE users;
+CREATE TABLE IF NOT EXISTS AvailableUnit(
+    unit_id int NOT NULL,
+    building_id int,
+    pm_id int,
+    room_num int NOT NULL,
+    lease_term int NOT NULL, -- measured in months or years?
+    num_beds int NOT NULL,
+    floor_num int NOT NULL,
+    num_washrooms int NOT NULL,
+    rent_price int NOT NULL,
+    PRIMARY KEY (unit_id),
+    FOREIGN KEY (building_id) REFERENCES Building(building_id),
+    FOREIGN KEY (pm_id) REFERENCES UnitListerAccount(pm_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS Review(
+    review_id int NOT NULL,
+    building_id int,
+    admin_helpfulness_rating int NOT NULL,
+    cleanliness_rating int NOT NULL,
+    comment TEXT NOT NULL,
+    review_helpfulness int NOT NULL,
+    PRIMARY KEY (review_id),
+    FOREIGN KEY (building_id) REFERENCES Building(building_id)
+);
+
+DESCRIBE UnitListerAccount;
+
+DESCRIBE Building;
+
+DESCRIBE AvailableUnit;
+
+DESCRIBE Review;
