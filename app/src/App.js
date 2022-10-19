c7ba977fe879809d857f26067b10b5b1405d315e
Martin Xing <martinx0520@gmail.com>
2022-10-19 17:41:21 -0400

    completed form toggle for creating post

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import { Button, Modal } from '@mui/material';
+import Postform from './createPost/postform';
 import './App.css';
+
 
 function App() {
 
   const [message, setMessage] = useState(0);
 
   const [dbMessage, setDbMessage] = useState(0);
+
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+  }
 
   useEffect(() => {
     fetch('/api').then(res => res.json()).then(data => {
@@ -35,6 +45,10 @@ function App() {
         >
           Learn React
         </a>
+        <Button color='primary' variant='h4' align='right' onClick={handleOpen}>{'Create A Post'}</Button>
+        <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description" >
+          <Postform handleClose={handleClose} />
+        </Modal>
         <p>{message}</p>
         <p>{dbMessage}</p>
       </header>
