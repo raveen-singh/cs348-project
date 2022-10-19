d2398d1795dc26539eea1496b2846f82adbc990c
Zaki Machfj <zakimachfj@gmail.com>
2022-10-19 15:26:46 -0400

    Adding username and password fields to UnitListerAccount (#6)
    * add username and password fields
    
    * update insert values in populate_tables
    
    * phone_num as string

--- api/create_tables.sql
+++ api/create_tables.sql
@@ -2,7 +2,9 @@ CREATE DATABASE IF NOT EXISTS cs348db;
 USE cs348db;
 
 CREATE TABLE IF NOT EXISTS UnitListerAccount(
-    pm_id int NOT NULL AUTO_INCREMENT,  
+    pm_id int NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     phone_num VARCHAR(10) NOT NULL,
     email VARCHAR(255) NOT NULL,
