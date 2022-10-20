48d730ec1ee414c0ea7904560ccc63995f4a0607
jennybai01 <55304664+jennybai01@users.noreply.github.com>
2022-10-20 00:33:04 -0400

    changes to sql

--- api/populate_tables.sql
+++ api/populate_tables.sql
@@ -7,6 +7,12 @@ VALUES (NULL,'330 Phillip St',1,'ensuite','apartment',0.7);
 INSERT INTO AvailableUnit
 VALUES (NULL, 1, 1, 202, 8, 4, 2, 'path/to/image', 2, 1000);
 
+INSERT INTO AvailableUnit
+VALUES (NULL, 1, 1, 101, 4, 3, 3, 'path/to/image', 3, 900);
+
+INSERT INTO AvailableUnit
+VALUES (NULL, 1, 1, 303, 12, 2, 4, 'path/to/image', 1, 800);
+
 INSERT INTO Review
 VALUES (NULL, 1, 5, 3, 'This is a comment.', 4);
 
