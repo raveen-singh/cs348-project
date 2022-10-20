d98b5e84968f272d1c61215433fbf08afa80884f
jennybai01 <55304664+jennybai01@users.noreply.github.com>
2022-10-20 00:57:05 -0400

    commit

--- api/create_tables.sql
+++ api/create_tables.sql
@@ -3,23 +3,25 @@ USE cs348db;
 
 CREATE TABLE IF NOT EXISTS UnitListerAccount(
     pm_id int NOT NULL AUTO_INCREMENT,
-    username VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     phone_num VARCHAR(10) NOT NULL,
     email VARCHAR(255) NOT NULL,
     website VARCHAR(255),
     PRIMARY KEY (pm_id),
+    UNIQUE(username)
 );
 
 CREATE TABLE IF NOT EXISTS Building(
     building_id int NOT NULL AUTO_INCREMENT,
-    address VARCHAR(255) UNIQUE NOT NULL,
+    address VARCHAR(255) NOT NULL,
     pet_friendly TINYINT(1) NOT NULL,
     laundry_availability ENUM('building', 'ensuite', 'none') NOT NULL,
     type_of_unit ENUM('apartment','house') NOT NULL,
     distance_from_waterloo DECIMAL(3,1) NOT NULL,
     PRIMARY KEY (building_id),
+    UNIQUE(address)
 );
 
 CREATE TABLE IF NOT EXISTS AvailableUnit(
