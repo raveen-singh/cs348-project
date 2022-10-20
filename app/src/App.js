0fe80aa936b680d063405b572ca109cb264d0ed8
Martin Xing <martinx0520@gmail.com>
2022-10-19 22:03:25 -0400

    save changes

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Button, Modal } from "@mui/material";
 
 // Components
 import Register from "./components/Register";
+import Postform from "./components/createPost/postform";
 
 // Just a dummy component to make navigating to the
 // sign up form easier for the feature
@@ -11,13 +13,39 @@ const Home = () => {
 };
 
 const App = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-      </Routes>
-    </Router>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+        </Routes>
+      </Router>
+      <Button
+        className="Form-button"
+        variant="contained"
+        color="primary"
+        size="large"
+        type="submit"
+        onClick={handleOpen}
+      >
+        Create A Post
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Postform handleClose={handleClose} />
+      </Modal>
+    </div>
   );
 };
 
