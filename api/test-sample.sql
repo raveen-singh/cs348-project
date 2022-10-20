ea0bb8c5f95d53e83dcb56b838dcb90bf435569a
jennybai01 <55304664+jennybai01@users.noreply.github.com>
2022-10-20 14:55:24 -0400

    added test-sample.sql

---
+++ api/test-sample.sql
@@ -0,0 +1,28 @@
+-- prior, make sure to run cleanup_tables.sql, create_tables.sql and populate_tables.sql
+
+
+-- feature 4: view available units with filter and sort functionality
+-- sample query 1
+SELECT u.name AS lister, u.email, u.phone_num, b.address, b.distance_from_waterloo, au.num_beds, au.num_washrooms, au.lease_term, au.rent_price, b.laundry_availability, b.pet_friendly, b.type_of_unit
+FROM AvailableUnit AS au 
+LEFT JOIN building AS b 
+ON au.building_id = b.building_id 
+LEFT JOIN UnitListerAccount AS u 
+ON au.pm_id = u.pm_id;
+-- sample query 2
+SELECT u.name AS lister, u.email, u.phone_num, b.address, b.distance_from_waterloo, au.num_beds, au.num_washrooms, au.lease_term, au.rent_price, b.laundry_availability, b.pet_friendly, b.type_of_unit
+FROM AvailableUnit AS au 
+LEFT JOIN building AS b 
+ON au.building_id = b.building_id 
+LEFT JOIN UnitListerAccount AS u 
+ON au.pm_id = u.pm_id
+WHERE num_washrooms > 1 and num_beds > 2;
+-- sample query 3
+SELECT u.name AS lister, u.email, u.phone_num, b.address, b.distance_from_waterloo, au.num_beds, au.num_washrooms, au.lease_term, au.rent_price, b.laundry_availability, b.pet_friendly, b.type_of_unit
+FROM AvailableUnit AS au 
+LEFT JOIN building AS b 
+ON au.building_id = b.building_id 
+LEFT JOIN UnitListerAccount AS u 
+ON au.pm_id = u.pm_id
+WHERE lease_term >= 8
+ORDER BY rent_price;
