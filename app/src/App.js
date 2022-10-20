85a3f2696d8ef0cf93d6c4efd08f74a85d4213fa
Zaki Machfj <zakimachfj@gmail.com>
2022-10-19 21:09:05 -0400

    delete unecessary files and fixup app.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,58 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import { Button, Modal } from '@mui/material';
-import Postform from './components/createPost/postform';
-import Post from './components/post/post';
-import './App.css';
-
+import React, { useState } from "react";
+import { Button, Modal } from "@mui/material";
+import Postform from "./components/createPost/postform";
+import Post from "./components/post/post";
+import "./App.css";
 
 function App() {
-
-  const [message, setMessage] = useState(0);
-
-  const [dbMessage, setDbMessage] = useState(0);
-
   const [open, setOpen] = useState(false);
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => {
     setOpen(false);
-  }
-
-  useEffect(() => {
-    fetch('/api').then(res => res.json()).then(data => {
-      setMessage(data.message);
-    });
-  }, []);
-
-  useEffect(() => {
-    fetch('/api/db').then(res => res.json()).then(data => {
-      setDbMessage(data.message);
-    });
-  }, []);
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button className="Form-button" variant="contained" color="primary" size="large" type="submit" onClick={handleOpen}>Create A Post</Button>
-        <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description" >
-          <Postform handleClose={handleClose} />
-        </Modal>
-        <p>{message}</p>
-        <p>{dbMessage}</p>
-      </header>
+      <Button
+        className="Form-button"
+        variant="contained"
+        color="primary"
+        size="large"
+        type="submit"
+        onClick={handleOpen}
+      >
+        Create A Post
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Postform handleClose={handleClose} />
+      </Modal>
     </div>
   );
 }
