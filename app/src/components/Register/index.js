3dfa9b6e9add1518fa89ff0c1bd23d6d13609e77
Zaki Machfj <zakimachfj@gmail.com>
2022-11-12 16:35:06 -0500

    change register form to use mui styles

--- app/src/components/Register/index.js
+++ app/src/components/Register/index.js
@@ -1,7 +1,17 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 import { useNavigate } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
 import axios from "axios";
-import "./index.css";
 
 const Register = () => {
   const [formValues, setFormValues] = useState({
@@ -13,11 +23,6 @@ const Register = () => {
     website: "",
   });
   const [message, setMessage] = useState("");
-  const userRef = useRef();
-
-  useEffect(() => {
-    userRef.current.focus();
-  }, []);
 
   const navigate = useNavigate();
 
@@ -49,60 +54,112 @@ const Register = () => {
     }
   };
 
+  useEffect(() => {
+    makeToast();
+  }, [message]);
+
+  const makeToast = () => message && toast.error(message);
+
   return (
-    <div className="container">
-      <h1 className="title">Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          id="username"
-          ref={userRef}
-          value={formValues.username}
-          onChange={handleChange}
-          placeholder="username"
-          required
-        />
-        <input
-          id="password"
-          type="password"
-          value={formValues.password}
-          onChange={handleChange}
-          placeholder="password"
-          required
-        />
-        <input
-          id="name"
-          value={formValues.name}
-          onChange={handleChange}
-          placeholder="name"
-          required
-        />
-        <input
-          id="phone_num"
-          value={formValues.phone_num}
-          onChange={handleChange}
-          placeholder="phone number"
-          required
-        />
-        <input
-          id="email"
-          type="email"
-          value={formValues.email}
-          onChange={handleChange}
-          placeholder="email"
-          required
-        />
-        <input
-          id="website"
-          value={formValues.url}
-          onChange={handleChange}
-          placeholder="website (optional)"
-        />
-        <button className="submit-form" type="submit">
-          Create Account
-        </button>
-      </form>
-      {message && <p className="err-message">{message}</p>}
-    </div>
+    <Container component="main" maxWidth="xs">
+      <Toaster />
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign Up
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                value={formValues.username}
+                onChange={handleChange}
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                value={formValues.name}
+                onChange={handleChange}
+                required
+                fullWidth
+                id="name"
+                label="Name"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                value={formValues.email}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                value={formValues.password}
+                onChange={handleChange}
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                id="password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="phone_num"
+                label="Phone Number"
+                value={formValues.phone_num}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="website"
+                label="Website (optional)"
+                value={formValues.website}
+                onChange={handleChange}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
