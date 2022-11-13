65dad10a0b14d3a9dd6bb9959c6ef47b207fa02c
Zaki Machfj <zmachfj@uwaterloo.ca>
2022-11-13 14:10:05 -0500

    Create Navbar component and add it as a layout (#14)
    * create navbar component
    
    * add navbar component to app
    
    * reset default css values

--- app/src/components/Home/index.js
+++ app/src/components/Home/index.js
@@ -1,36 +1,9 @@
-import React, { useState } from "react";
-import UnitForm from "../UnitForm";
-import { Button, Modal } from "@mui/material";
-import { Link } from "react-router-dom";
+import React from "react";
 
 const Home = () => {
-  const [open, setOpen] = useState(false);
-
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
   return (
     <div>
-      <Link to="/register">Sign Up</Link>
-      <br />
-      <Button
-        sx={{ marginTop: "1rem" }}
-        className="Form-button"
-        variant="contained"
-        color="primary"
-        size="large"
-        type="submit"
-        onClick={handleOpen}
-      >
-        Create A Post
-      </Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <UnitForm handleClose={handleClose} />
-      </Modal>
+      {/* In the future, there will be 2 buttons for viewing units or buildings */}
     </div>
   );
 };
---
+++ app/src/components/Navbar/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import { Link as RouterLink } from "react-router-dom";
+
+import Modal from "@mui/material/Modal";
+import UnitForm from "../UnitForm";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="absolute">
+        <Toolbar>
+          <Button
+            variant="filled"
+            component={RouterLink}
+            sx={{ display: "inline", width: "auto" }}
+            to="/"
+          >
+            Home
+          </Button>
+          <Button variant="filled" component={RouterLink} to="/register">
+            Sign Up
+          </Button>
+          <Button variant="filled" onClick={handleOpen}>
+            Create A Post
+          </Button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <UnitForm handleClose={handleClose} />
+          </Modal>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Navbar;
