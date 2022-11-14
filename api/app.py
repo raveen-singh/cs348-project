8ddfb2c1fabcff381b9be1f7c1676ecd1da7f34e
Jenny Bai <55304664+jennybai01@users.noreply.github.com>
2022-11-13 19:46:59 -0500

    Sql fetch dict (#17)
    * unique UnitListerAccount.username and Building.address
    
    * changes to sql
    
    * commit

--- api/app.py
+++ api/app.py
@@ -10,6 +10,7 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'cs348db'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
 
