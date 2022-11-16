e4dda674ce28f27ef1120738c9eed3db63b980c7
Zaki Machfj <zakimachfj@gmail.com>
2022-11-16 01:38:26 -0500

    style review

--- app/src/components/ReviewList/index.js
+++ app/src/components/ReviewList/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import { Stack } from "@mui/system";
+import Stack from "@mui/material/Stack";
+import Rating from "@mui/material/Rating";
+import IconButton from "@mui/material/IconButton";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 
 const ReviewList = ({ reviews, address }) => {
   return (
@@ -12,14 +15,41 @@ const ReviewList = ({ reviews, address }) => {
       {reviews ? (
         <Stack spacing={3}>
           {reviews.map((review) => (
-            <Box
+            <Stack
               key={review.review_id}
               p={2}
               border="1px solid #Dde6ea"
               borderRadius={2}
+              spacing={1}
+              position="relative"
             >
-              <Typography>{review.comment}</Typography>
-            </Box>
+              <Stack className="reviews" direction="row" spacing={2}>
+                <Box display="flex" alignContent="center">
+                  <Typography mr={1}>Cleanliness</Typography>
+                  <Rating
+                    size="small"
+                    readOnly
+                    value={review.cleanliness_rating}
+                  />
+                </Box>
+                <Box display="flex" alignContent="center">
+                  <Typography mr={1}>Admin Helpfulness</Typography>
+                  <Rating
+                    size="small"
+                    readOnly
+                    value={review.admin_helpfulness}
+                  />
+                </Box>
+              </Stack>
+              <Typography>"{review.comment}"</Typography>
+              <Typography>- Happy Hippo</Typography>
+              <IconButton
+                color="primary"
+                sx={{ position: "absolute", bottom: 4, right: 4 }}
+              >
+                <ThumbUpIcon />
+              </IconButton>
+            </Stack>
           ))}
         </Stack>
       ) : (
