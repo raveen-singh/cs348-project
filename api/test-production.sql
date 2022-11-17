74ac7a156a697b93361aed6d9c27d3aaf220b15b
jennybai01 <55304664+jennybai01@users.noreply.github.com>
2022-11-17 16:41:33 -0500

    test-production.sql .out

--- api/test-production.sql
+++ api/test-production.sql
@@ -21,7 +21,8 @@ FROM AvailableUnit AS au
 LEFT JOIN building AS b 
 ON au.building_id = b.building_id 
 LEFT JOIN UnitListerAccount AS u 
-ON au.account_id = u.account_id;
+ON au.account_id = u.account_id
+LIMIT 10;
 -- sample query 2
 SELECT u.name AS lister, u.email, u.phone_num, b.address, b.distance_from_waterloo, au.num_beds, au.num_washrooms, au.lease_term, au.rent_price, b.laundry_availability, b.pet_friendly, b.type_of_unit
 FROM AvailableUnit AS au 
@@ -29,7 +30,8 @@ LEFT JOIN building AS b
 ON au.building_id = b.building_id 
 LEFT JOIN UnitListerAccount AS u 
 ON au.account_id = u.account_id
-WHERE num_washrooms > 1 and num_beds > 2;
+WHERE num_washrooms > 1 and num_beds > 2
+LIMIT 10;
 -- sample query 3
 SELECT u.name AS lister, u.email, u.phone_num, b.address, b.distance_from_waterloo, au.num_beds, au.num_washrooms, au.lease_term, au.rent_price, b.laundry_availability, b.pet_friendly, b.type_of_unit
 FROM AvailableUnit AS au 
@@ -38,7 +40,8 @@ ON au.building_id = b.building_id
 LEFT JOIN UnitListerAccount AS u 
 ON au.account_id = u.account_id
 WHERE lease_term >= 8
-ORDER BY rent_price;
+ORDER BY rent_price
+LIMIT 10;
 
 -- feature 5: view building reviews with filter and sort functionality
 -- sample query 1
@@ -46,7 +49,8 @@ SELECT b.building_id, b.address, AVG(r.admin_helpfulness_rating) AS average_admi
 FROM Review AS r 
 INNER JOIN building AS b
 ON r.building_id = b.building_id 
-GROUP BY b.building_id;
+GROUP BY b.building_id
+LIMIT 10;
 
 -- sample query 2
 SELECT b.building_id, b.address, AVG(r.admin_helpfulness_rating) AS average_admin_helpfulness_rating, AVG(r.cleanliness_rating) AS average_cleanliness_rating, AVG(r.review_helpfulness) AS average_review_helpfulness, b.distance_from_waterloo, b.laundry_availability, b.pet_friendly, b.type_of_unit
@@ -54,7 +58,8 @@ FROM Review AS r
 INNER JOIN building AS b
 ON r.building_id = b.building_id 
 GROUP BY b.building_id
-HAVING AVG(r.review_helpfulness) > 4;
+HAVING AVG(r.review_helpfulness) > 4
+LIMIT 10;
 
 -- sample query 3
 SELECT b.building_id, b.address, AVG(r.admin_helpfulness_rating) AS average_admin_helpfulness_rating, AVG(r.cleanliness_rating) AS average_cleanliness_rating, AVG(r.review_helpfulness) AS average_review_helpfulness, b.distance_from_waterloo, b.laundry_availability, b.pet_friendly, b.type_of_unit
@@ -62,7 +67,8 @@ FROM Review AS r
 INNER JOIN building AS b
 ON r.building_id = b.building_id 
 WHERE laundry_availability = 'ensuite'
-GROUP BY b.building_id;
+GROUP BY b.building_id
+LIMIT 10;
 
 -- sample query 4
 SELECT b.building_id, b.address, AVG(r.admin_helpfulness_rating) AS average_admin_helpfulness_rating, AVG(r.cleanliness_rating) AS average_cleanliness_rating, AVG(r.review_helpfulness) AS average_review_helpfulness, b.distance_from_waterloo, b.laundry_availability, b.pet_friendly, b.type_of_unit
@@ -71,7 +77,8 @@ INNER JOIN building AS b
 ON r.building_id = b.building_id 
 GROUP BY b.building_id
 HAVING AVG(r.admin_helpfulness_rating) >= 3
-ORDER BY AVG(r.review_helpfulness) DESC;
+ORDER BY AVG(r.review_helpfulness) DESC
+LIMIT 10;
 
 -- feature 6: 
 DELETE FROM AvailableUnit WHERE unit_id = 1;
