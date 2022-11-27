ddc72ce3e36fb9f0ba7c9650c586a53e13c32f07
jennybai01 <55304664+jennybai01@users.noreply.github.com>
2022-11-27 15:38:13 -0500

    commit

--- api/app.py
+++ api/app.py
@@ -251,7 +251,7 @@ def get_review():
 
     return {"success": True, "reviews": reviews}
 
-@app.route('/api/reviews/get_random_name', methods = ["GET"])
+@app.route('/api/reviews/get_random_names', methods = ["GET"])
 def get_random_name():
     json_data = request.get_json()
     num = json_data["number_of_names"]
