f189fd6c8069c61a3fa2a08961466be2f5f221b2
raveen-singh <18638226+raveen-singh@users.noreply.github.com>
2022-11-28 19:46:26 -0500

    Revert "Unit changes (#45)" (#47)
    This reverts commit d41d0465a153e8575bd7f87c449971307709a2e6.
    
    Co-authored-by: Jenny Bai <55304664+jennybai01@users.noreply.github.com>

--- app/src/components/UnitList/index.js
+++ app/src/components/UnitList/index.js
@@ -5,28 +5,10 @@ import axios from "axios";
 import UnitForm from "../UnitForm";
 import UnitCard from "../UnitCard";
 
-const defaultFetchedUnitValues = {
-  address: "",
-  room_num: "",
-  rent_price: 500,
-  num_beds: 5,
-  num_washrooms: 5,
-  lease_term: 4,
-  floor_num: "",
-  image_path: "",
-  new_address: "",
-  pet_friendly: 0,
-  laundry_availability: "Building",
-  type_of_unit: "Apartment",
-  distance_from_waterloo: "0.0",
-};
-
 const UnitList = () => {
   const [open, setOpen] = useState(false);
   const [addresses, setAddresses] = useState({});
   const [units, setUnits] = useState([]);
-  const [unitId, setUnitId] = useState(null);
-  const [editPost, setEditPost] = useState(defaultFetchedUnitValues);
 
   const getUnits = async () => {
     try {
@@ -36,22 +18,6 @@ const UnitList = () => {
       console.log(error);
     }
   };
-
-  const getunitData = async () => {
-    const { data }  = await axios.get(`/api/unit/get?id=${unitId}`);
-    console.log(data.data[0]);
-    Object.keys(data.data[0]).forEach((key) => {
-      if (editPost.hasOwnProperty(key)) {
-       editPost[key] = data.data[0][key]
-      }
-    });
-    for (const [key, value] of Object.entries(addresses)) {
-      if (value === data.data[0].building_id) {
-        editPost["address"] = key;
-      }
-    }
-    handleOpen();
-  }
 
   const getAddresses = async () => {
     try {
@@ -69,22 +35,9 @@ const UnitList = () => {
     getUnits();
   }, []);
 
-  useEffect(() => {
-    if (unitId) {
-      getunitData();
-    }
-  }, [unitId])
-  
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
   const handleClose = () => {
     setOpen(false);
-    setUnitId(null);
-    setEditPost(defaultFetchedUnitValues);
   };
-
 
   return (
     <div>
@@ -94,7 +47,7 @@ const UnitList = () => {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <UnitForm handleClose={handleClose} addressDict={addresses} unitId={unitId} setUnitId={setUnitId} editPost={editPost} setEditPost={setEditPost} />
+        <UnitForm handleClose={handleClose} addressDict={addresses} />
       </Modal>
       <Box sx={{ width: "90%", margin: "100px 5%" }}>
         <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 4 }}>
@@ -104,7 +57,7 @@ const UnitList = () => {
             <>
               {units.map((unit) => (
                 <Grid item xs={12} sm={6} md={4}>
-                  <UnitCard unit={unit} addressDict={addresses} setUnitId={setUnitId} />
+                  <UnitCard unit={unit} addressDict={addresses} />
                 </Grid>
               ))}
             </>
