6e8e48ee7d521e82d2b1212b53a8ae86242d25e0
denishu <denishu17186@gmail.com>
2022-11-28 23:50:16 -0500

    switching to geolocation api

--- app/src/components/MapsCard/index.js
+++ app/src/components/MapsCard/index.js
@@ -4,6 +4,8 @@ import CardContent from "@mui/material/CardContent";
 import { useMemo } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
+const google = window.google;
+
 export default function BaseMap(building) {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyBEp1Ikww7LTAnj8Gzew1jY555INnUbArQ",
@@ -14,10 +16,10 @@ export default function BaseMap(building) {
 }
 
 function Map(building) {
+
+  const geocoder = google.maps.Geocoder;
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
-
-  const geocoder = new google.maps.Geocoder();
 
   geocoder.geocode({ address: building.address }).then((result) => {
     const { results } = result;
@@ -31,7 +33,7 @@ function Map(building) {
       <CardContent>
         <GoogleMap
           zoom={10}
-          center={{ lat, lng }}
+          center={{ latitude, longitude }}
           mapContainerClassName={{ width: "100%", height: "100%" }}
         >
           <Marker position={center} />
