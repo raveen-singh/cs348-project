0d8893ee7a9165e4837b75ffcdaef2ce11f45ee8
denishu <39774395+denishu@users.noreply.github.com>
2022-11-29 16:45:46 -0500

    Maps (#52)
    * README updated for files
    
    * backend route for get my units
    
    * maps, still working on lat/lng
    
    * add code
    
    * add code
    
    * fix typo
    
    * switching to geolocation api
    
    * remove
    
    * log
    
    * fix hooks
    
    * useEffect changes
    
    * fix useEffect
    
    * copy of working google maps with constants for coords
    
    * dynamic coords
    
    * fix
    
    * wait for done
    
    * debugging maps
    
    * remove comments
    
    * add missing line
    
    * remove logs
    
    Co-authored-by: Zaki Machfj <zakimachfj@gmail.com>
    Co-authored-by: jennybai01 <55304664+jennybai01@users.noreply.github.com>
    Co-authored-by: raveen-singh <18638226+raveen-singh@users.noreply.github.com>

---
+++ app/src/components/MapsCard/index.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
+import axios from "axios";
+import { useEffect } from "react";
+
+export default function BaseMap({ building }) {
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const [done, setDone] = useState(false);
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyBEp1Ikww7LTAnj8Gzew1jY555INnUbArQ",
+  });
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${building.address}&key=AIzaSyD97JXp2t36bmAvNtSUibldGBs08Z4cHxE`
+      );
+      setLatitude(data.results[0].geometry.location.lat);
+      setLongitude(data.results[0].geometry.location.lng);
+      setDone(true);
+    })();
+  }, []);
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Map lat={latitude} lng={longitude} />;
+}
+
+function Map({ lat, lng }) {
+  return (
+    <Card>
+      <CardContent>
+        <GoogleMap
+          zoom={16}
+          center={{ lat, lng }}
+          mapContainerStyle={{ width: "600px", height: "300px" }}
+        >
+          <MarkerF position={{ lat, lng }} />
+        </GoogleMap>
+      </CardContent>
+    </Card>
+  );
+}
