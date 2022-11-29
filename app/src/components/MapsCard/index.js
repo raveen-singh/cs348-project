93e8548188d21af165105b85a4089ac51f328f6f
Zaki Machfj <zakimachfj@gmail.com>
2022-11-28 21:53:01 -0500

    add code

--- app/src/components/MapsCard/index.js
+++ app/src/components/MapsCard/index.js
@@ -1,38 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import { useMemo } from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"
-
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
 export default function BaseMap(building) {
-    const { isLoaded } = useLoadScript({
-        googleMapsApiKey: "AIzaSyBEp1Ikww7LTAnj8Gzew1jY555INnUbArQ"
-    });
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyBEp1Ikww7LTAnj8Gzew1jY555INnUbArQ",
+  });
 
-    if (!isLoaded) return <div>Loading...</div>
-    return Map(building);
+  if (!isLoaded) return <div>Loading...</div>;
+  return Map(building);
 }
 
 function Map(building) {
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode( {'address': building.address})
-    var latitude = building.address.geometry.location.lat();
-    var longitude = building.address.geometry.location.lng();
-    const center = useMemo(() => ({ lat: latitude, lng: longitude}), []);
-    //const MapsCard = ({building}) => {
-        return (
-            <Card>
-                <CardContent>
-                    <GoogleMap 
-                        zoom={10} 
-                        center={{ lat: latitude, lng: longitude }} 
-                        mapContainerClassName={{width: '100%', height: '100%'}}
-                    >
-                        <Marker position={center} />
-                    </GoogleMap>
-                </CardContent>
-            </Card>
-        )
-    //}
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+
+  const geocoder = new google.maps.Geocoder();
+
+  geocoder.geocode({ address: building.address }).then((result) => {
+    const { results } = result;
+    setLatitude(results[0].geometry.location.lat);
+    setLongitude(results[0].geometry.location.lng);
+  });
+
+  const center = useMemo(() => ({ lat: latitude, lng: longitude }), []);
+  return (
+    <Card>
+      <CardContent>
+        <GoogleMap
+          zoom={10}
+          center={{ lat, lng }}
+          mapContainerClassName={{ width: "100%", height: "100%" }}
+        >
+          <Marker position={center} />
+        </GoogleMap>
+      </CardContent>
+    </Card>
+  );
+  //}
 }
