99acf45f8b47891d63367853b31343c39a537b6b
Zaki Machfj <zakimachfj@gmail.com>
2022-11-29 01:43:56 -0500

    wait for done

--- app/src/components/MapsCard/index.js
+++ app/src/components/MapsCard/index.js
@@ -8,6 +8,7 @@ import { useEffect } from "react";
 export default function BaseMap({ building }) {
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
+  const [done, setDone] = useState(false);
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyBEp1Ikww7LTAnj8Gzew1jY555INnUbArQ",
@@ -19,12 +20,13 @@ export default function BaseMap({ building }) {
         `https://maps.googleapis.com/maps/api/geocode/json?address=${building.address}&key=AIzaSyD97JXp2t36bmAvNtSUibldGBs08Z4cHxE`
       );
       console.log(data);
-      setLatitude(data.results[0].geometry.location.lat ?? 0);
-      setLongitude(data.results[0].geometry.location.lng ?? 0);
+      setLatitude(data.results[0].geometry.location.lat);
+      setLongitude(data.results[0].geometry.location.lng);
+      setDone(true);
     })();
   }, []);
 
-  if (!isLoaded) return <div>Loading...</div>;
+  if (!isLoaded || !done) return <div>Loading...</div>;
   return <Map lat={latitude} lng={longitude} />;
 }
 
