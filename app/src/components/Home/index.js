81879639b3dcf0d4be0481a3470a7d00f0a183c1
Zaki Machfj <zakimachfj@gmail.com>
2022-11-28 19:40:20 -0500

    add simple home page

--- app/src/components/Home/index.js
+++ app/src/components/Home/index.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { Typography, Button, Box } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import Apartment from "@mui/icons-material/Apartment";
 
 const Home = () => {
   return (
-    <div>
-      {/* In the future, there will be 2 buttons for viewing units or buildings */}
-    </div>
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="80vh"
+      px={20}
+    >
+      <Typography fontSize="3rem" textAlign="center" mb={2}>
+        Welcome to UWHome!
+      </Typography>
+      <Box>
+        <Button variant="contained" startIcon={<Apartment />} sx={{ mr: 2 }}>
+          View Buildings
+        </Button>
+        <Button variant="contained" startIcon={<HomeIcon />}>
+          View Units
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
