393bc2d88a43c497bbe03a0908dabb03e550c7e2
Jenny Bai <55304664+jennybai01@users.noreply.github.com>
2022-11-30 22:49:19 -0500

    Change queries (#56)
    * change queries in test-sample and test-production
    
    * changed test-sample and test-production
    
    Co-authored-by: Zaki Machfj <zakimachfj@gmail.com>

--- api/test-sample.sql
+++ api/test-sample.sql
@@ -29,7 +29,7 @@ LEFT JOIN building AS b
 ON au.building_id = b.building_id 
 LEFT JOIN UnitListerAccount AS u 
 ON au.account_id = u.account_id
-WHERE num_washrooms > 1 and num_beds > 2;
+WHERE num_washrooms = 1;
 -- sample query 3
 SELECT u.name AS lister, u.email, u.phone_num, b.address, b.distance_from_waterloo, au.num_beds, au.num_washrooms, au.lease_term, au.rent_price, b.laundry_availability, b.pet_friendly, b.type_of_unit
 FROM AvailableUnit AS au 
@@ -37,41 +37,25 @@ LEFT JOIN building AS b
 ON au.building_id = b.building_id 
 LEFT JOIN UnitListerAccount AS u 
 ON au.account_id = u.account_id
-WHERE lease_term >= 8
+WHERE lease_term = 8
 ORDER BY rent_price;
 
--- feature 5: view building reviews with filter and sort functionality
--- sample query 1
-SELECT b.building_id, b.address, AVG(r.admin_helpfulness_rating) AS average_admin_helpfulness_rating, AVG(r.cleanliness_rating) AS average_cleanliness_rating, AVG(r.review_helpfulness) AS average_review_helpfulness, b.distance_from_waterloo, b.laundry_availability, b.pet_friendly, b.type_of_unit
-FROM Review AS r 
-INNER JOIN building AS b
-ON r.building_id = b.building_id 
-GROUP BY b.building_id;
+-- feature 5: view building reviews (all reviews and average)
+-- sample query 1 for viewing average reviews for all buildings
+SELECT b.building_id, r.admin_rating, r.cleanliness_rating, b.distance_from_waterloo, b.laundry_availability, b.pet_friendly, b.type_of_unit 
+FROM Building b 
+LEFT JOIN (
+    SELECT building_id, ROUND(AVG(admin_helpfulness_rating), 1) AS admin_rating, ROUND(AVG(cleanliness_rating), 1) AS cleanliness_rating 
+    FROM Review 
+    GROUP BY building_id
+) r
+ON b.building_id = r.building_id;
 
--- sample query 2
-SELECT b.building_id, b.address, AVG(r.admin_helpfulness_rating) AS average_admin_helpfulness_rating, AVG(r.cleanliness_rating) AS average_cleanliness_rating, AVG(r.review_helpfulness) AS average_review_helpfulness, b.distance_from_waterloo, b.laundry_availability, b.pet_friendly, b.type_of_unit
-FROM Review AS r 
-INNER JOIN building AS b
-ON r.building_id = b.building_id 
-GROUP BY b.building_id
-HAVING AVG(r.review_helpfulness) > 4;
-
--- sample query 3
-SELECT b.building_id, b.address, AVG(r.admin_helpfulness_rating) AS average_admin_helpfulness_rating, AVG(r.cleanliness_rating) AS average_cleanliness_rating, AVG(r.review_helpfulness) AS average_review_helpfulness, b.distance_from_waterloo, b.laundry_availability, b.pet_friendly, b.type_of_unit
-FROM Review AS r 
-INNER JOIN building AS b
-ON r.building_id = b.building_id 
-WHERE laundry_availability = 'ensuite'
-GROUP BY b.building_id;
-
--- sample query 4
-SELECT b.building_id, b.address, AVG(r.admin_helpfulness_rating) AS average_admin_helpfulness_rating, AVG(r.cleanliness_rating) AS average_cleanliness_rating, AVG(r.review_helpfulness) AS average_review_helpfulness, b.distance_from_waterloo, b.laundry_availability, b.pet_friendly, b.type_of_unit
-FROM Review AS r 
-INNER JOIN building AS b
-ON r.building_id = b.building_id 
-GROUP BY b.building_id
-HAVING AVG(r.admin_helpfulness_rating) >= 3
-ORDER BY AVG(r.review_helpfulness) DESC;
+-- sample query 2 for viewing all reviews for a single building
+SELECT review_id, admin_helpfulness_rating, cleanliness_rating, review_helpfulness, comment
+FROM Review r
+LEFT JOIN building b ON r.building_id = b.building_id
+WHERE b.building_id = 1;
 
 -- feature 6: 
 DELETE FROM AvailableUnit WHERE unit_id = 1;
